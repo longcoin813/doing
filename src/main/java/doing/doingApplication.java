6cbb80dfb486f93d1790312f0841d60918246acd
Longcoin <Long>
2022-09-22 13:41:36 +0700

    update

--- src/main/java/doing/doingApplication.java
+++ src/main/java/doing/doingApplication.java
@@ -1,2 +1,13 @@
-package doing;public class doingApplication {
+package doing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class doingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(doingApplication.class, args);
+    }
+
 }
